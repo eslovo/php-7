661089e36cc357bd123974b1b80dbe7f4ff3189e
Vitaly Lebedev <eslovo@gmail.com>
2019-06-02 23:37:28 +0300

    HTML body parts: header and footer

---
+++ layout/footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etok
+ * Date: 02.06.2019
+ * Time: 23:36
+ */
+?>
+
+</body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etok
+ * Date: 02.06.2019
+ * Time: 23:35
+ */
+?>
+
+
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
