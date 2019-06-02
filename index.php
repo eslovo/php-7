907820985a8fae2e4dbd3bc86dd03636dd78a899
Vitaly Lebedev <eslovo@gmail.com>
2019-06-02 22:39:53 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Lebedev Vitaly
+ * Email: eslovo@gmail.com
+ * Date: 02.06.2019 17:19
+ */
+
+//error_reporting(E_ALL); ini_set('display_errors', 1);
+//die(__FILE__); //phpinfo();
+
+require_once dirname(__FILE__).'/listFiles.php';
